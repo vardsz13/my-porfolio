749a5b75eb65432db5e415c9809a451d3faacaa9
John Paul Varde <johnpaulvarde13@gmail.com>
2025-05-25 15:09:50 +0800

    Finalized the Ecommerce Projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,10 +1,12 @@
 import { Project } from '@/types/project';
+// Porfolio Project
 import portfolioImage from '../assets/projects/porfolio-website/portfolio-website.png';
 import portfolioResponmsiveImage from '../assets/projects/porfolio-website/portfolio-responsive-design.png';
 import portfolioDarkModeImage from '../assets/projects/porfolio-website/portfolio-dark-light-mode.png';
 import portfolioTransitionsImage from '../assets/projects/porfolio-website/portfolio-page-transitions.png';
 import portfolioContactImage from '../assets/projects/porfolio-website/portfolio-contact-form.png';
 import portfolioInteractiveImage from '../assets/projects/porfolio-website/portfolio-interactive-elements.png';
+// E-commerce Project
 import ecommerceImage from '../assets/projects/ecommerce-website/ecommerce-website.png';
 import ecommerceHomeImage from '../assets/projects/ecommerce-website/ecommerce-featured.png';
 import ecommerceProductsImage from '../assets/projects/ecommerce-website/ecommerce-items.png';
@@ -12,13 +14,13 @@ import ecommerceContactImage from '../assets/projects/ecommerce-website/ecommerc
 import ecommerceAboutImage from '../assets/projects/ecommerce-website/ecommerce-about-us-module.png';
 import ecommerceCartImage from '../assets/projects/ecommerce-website/ecommerce-cart.png';
 import ecommerceOrderSummaryImage from '../assets/projects/ecommerce-website/ecommerce-summary-orders.png';
-import ecommerceAdminImage from '../assets/projects/ecommerce-website/ecommerce-admin.png';
-import ecommerceDashboardImage from '../assets/projects/ecommerce-website/ecommerce-dashboard.png';
-import ecommerceProductManagementImage from '../assets/projects/ecommerce-website/ecommerce-product-management.png';
-import ecommerceTransactionManagementImage from '../assets/projects/ecommerce-website/ecommerce-transaction-management.png';
-import ecommerceAdminContactImage from '../assets/projects/ecommerce-website/ecommerce-admin-contact.png';
-import ecommerceOrderReportsImage from '../assets/projects/ecommerce-website/ecommerce-order-reports.png';
-
+import ecommerceAdminImage from '../assets/projects/ecommerce-website/ecommerce-admin-login.png';
+import ecommerceDashboardImage from '../assets/projects/ecommerce-website/ecommerce-admin-dashboard.png';
+import ecommerceProductManagementImage from '../assets/projects/ecommerce-website/ecommerce-admin-products.png';
+import ecommerceTransactionManagementImage from '../assets/projects/ecommerce-website/ecommerce-admin-transaction.png';
+import ecommerceUserManagementImage from '../assets/projects/ecommerce-website/ecommerce-admin-users.png';
+import ecommerceOrderReportsImage from '../assets/projects/ecommerce-website/ecommerce-admin-orders.png';
+// IoT Mobile App Project
 import iotImage from '../assets/projects/iot-mobile-app/iot-mobile-app.png';
 import iotHomeModuleImage from '../assets/projects/iot-mobile-app/iot-home-module.png';
 import iotSensorModuleImage from '../assets/projects/iot-mobile-app/iot-sensor-module.png'; 
@@ -26,10 +28,12 @@ import iotAlarmStateModuleImage from '../assets/projects/iot-mobile-app/iot-alar
 import iotHardwareIntegrationImage from '../assets/projects/iot-mobile-app/iot-hardware-integration.png';
 import iotContactModuleImage from '../assets/projects/iot-mobile-app/iot-contact-module.png';
 import iotAboutModuleImage from '../assets/projects/iot-mobile-app/iot-about-module.png';
+// File Management System Capstone Project
 import fileManagementImage from '../assets/projects/file-management-system/file-management-system.png';
 
 
 export const projects: Project[] = [
+  // Portfolio Project
   {
     id: "portfolio-website", 
     title: "Modern Portfolio Website",
@@ -68,6 +72,7 @@ export const projects: Project[] = [
       },
     ]
   },
+  // File Management System Capstone Project
   {
     id: "file-management-system",
     title: "Web-based File Management System",
@@ -96,6 +101,7 @@ export const projects: Project[] = [
       },
     ]
   },
+  // IoT Mobile App Project
   {
     id: "iot-mobile-app",
     title: "Mobile App with IoT Technologies",
@@ -139,6 +145,7 @@ export const projects: Project[] = [
       },
     ]
   },
+  // E-commerce Project
   {
     id: "ecommerce-website",
     title: "E-commerce Website",
@@ -183,32 +190,32 @@ export const projects: Project[] = [
        {
         title: "Admin Portal",
         description: "A secure area for staff to manage products, orders, and customer information.",
-        imageUrl: "/images/projects/ecom-cart.jpg",
+        imageUrl: ecommerceAdminImage,
       },
        {
         title: "Dashboard",
         description: "Gives admins a quick overview of sales and product performance with easy-to-read charts.",
-        imageUrl: "/images/projects/ecom-cart.jpg",
+        imageUrl: ecommerceDashboardImage,
       },
        {
         title: "Product Management",
         description: "Admins can add, edit, or update products and keep stock levels accurate.",
-        imageUrl: "/images/projects/ecom-cart.jpg",
+        imageUrl: ecommerceProductManagementImage,
       },
        {
         title: "Transaction Management",
         description: "Admins can view, confirm, or cancel orders to keep everything running smoothly.",
-        imageUrl: "/images/projects/ecom-cart.jpg",
+        imageUrl: ecommerceTransactionManagementImage,
       },
        {
-        title: "Admin Contact",
-        description: "Admins can see and respond to customer messages sent through the contact form.",
-        imageUrl: "/images/projects/ecom-cart.jpg",
+        title: "User Module",
+        description: "Admins can view and manage customer information, interactions, and account status in one place.",
+        imageUrl: ecommerceUserManagementImage,
       },
        {
         title: "Order Reports",
-        description: "Admins can track sales and revenue with easy-to-understand reports.",
-        imageUrl: "/images/projects/ecom-cart.jpg",
+        description: "Admins can easily see sales and earnings through simple, clear reports.",
+        imageUrl: ecommerceOrderReportsImage,
       },
     ]
   },
