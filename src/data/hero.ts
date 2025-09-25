16babc86ca33a985790f3ccca2f52998a5d51762
John Paul Varde <johnpaulvarde13@gmail.com>
2025-09-26 02:48:26 +0800

    change gmail

--- src/data/hero.ts
+++ src/data/hero.ts
@@ -5,7 +5,7 @@ export const heroData: HeroData = {
   name: "John Paul Varde",
   location: "Quezon City, Philippines",
   title: "Front-End Developer and UI/UX Designer",
-  email: "johnpaulvarde13@gmail.com",
+  email: "paul.varde10@gmail.com",
   profileImage: profilePic,
   availableToWork: true
 };
