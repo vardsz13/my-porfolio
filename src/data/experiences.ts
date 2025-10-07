2b95c96549599d8e5f4b2fc043984027ca0ef85a
John Paul Varde <johnpaulvarde13@gmail.com>
2025-10-07 20:31:12 +0800

    final update

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -2,7 +2,7 @@ import { ExperienceItem } from "@/types/experience";
 
 export const experiences: ExperienceItem[] = [
     {
-    title: "Freelance UI & Front-End Developer",
+    title: "Freelance UI/UX & Front-End Developer",
     company: "Freelance",
     year: "2025",
     description: [
@@ -19,6 +19,16 @@ export const experiences: ExperienceItem[] = [
       "Developed a Document Management System for a private organization to manage and organize digital documents.",
       "Followed ISO/IEC 25010 software quality standards to ensure usability, reliability, and maintainability.",
       "Gained hands-on experience in UI building, agile development practices, and teamwork within a corporate IT environment.",
+    ],
+  },
+   {
+    title: "Personalized Internet Ads Assessor",
+    company: "TELUS International",
+    year: "2023",
+    description: [
+      "Reviewed and optimized 1,000+ online ads for relevance and accuracy.",
+      "Maintained 98% compliance with quality assurance standards.",
+      "Gave feedback on ad content to help improve the quality of search results.",
     ],
   },
 ];
